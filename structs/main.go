e2eafeec8e17ae3c8f0314b49104193db046d898
Matthew Mccarthy <matthewmc96@gmail.com>
2020-12-09 12:09:09 -0600

    adding structs

---
+++ structs/main.go
@@ -0,0 +1,16 @@
+package main
+
+// Define person struct
+type Person struct {
+	firstName string
+	lastName  string
+	city      string
+	gender    string
+	age       string
+}
+
+func main() {
+	// Init person using struct
+	person1 := Person
+
+}

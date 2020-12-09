a998c98a4d0a8cd447d014c6ffe9c91174ee9240
Matthew Mccarthy <matthewmc96@gmail.com>
2020-12-09 00:08:14 -0600

    range and index

---
+++ 09_range/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	ids := []int{33, 76, 54, 23, 11, 2}
+
+	// Loop through ids
+	for i, id := range ids {
+		fmt.Printf("%d - ID: %d\n", i, id)
+	}
+
+	// Not using index
+	for _, id := range ids {
+		fmt.Printf("ID: %d\n", id)
+	}
+
+	// Add ids
+	sum := 0
+	for _, id := range ids {
+		sum += id
+	}
+	fmt.Println("Sum", sum)
+}

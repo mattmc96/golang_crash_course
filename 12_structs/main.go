5cfd86e2be3f6ab3e02e8f35d08333ed5ddc2f47
Matthew Mccarthy <matthewmc96@gmail.com>
2020-12-09 12:35:32 -0600

    finished structs

--- 12_structs/main.go
+++ 12_structs/main.go
@@ -7,11 +7,14 @@ import (
 
 // Define person struct
 type Person struct {
-	firstName string
-	lastName  string
-	city      string
-	gender    string
-	age       int
+	//firstName string
+	//lastName  string
+	//city      string
+	//gender    string
+	//age       int
+
+	firstName, lastName, city, gender string
+	age                               int
 }
 
 // Greeting method (value receiver)
@@ -24,16 +27,30 @@ func (p *Person) hasBirthday() {
 	p.age++
 }
 
+// getMarried (pointer receiver)
+func (p *Person) getMarried(spouseLastName string) {
+	if p.gender == "m" {
+		return
+	} else {
+		p.lastName = spouseLastName
+	}
+}
+
 func main() {
 	// Init person using struct
 	person1 := Person{firstName: "Samantha", lastName: "Smith", city: "Boston", gender: "f", age: 25}
 
-	//person1 := Person{"Samantha", "Smith", "Boston", "f", 5}
+	// Alternative
+	person2 := Person{"Bob", "Johnson", "NYC", "m", 30}
 
 	//fmt.Println(person1)
 	//fmt.Println(person1.firstName)
 	//person1.age++
 
 	person1.hasBirthday()
-	fmt.Println(person1.greet())
+	person1.getMarried("Williams")
+
+	person2.getMarried("Thompson")
+
+	fmt.Println(person2.greet())
 }
